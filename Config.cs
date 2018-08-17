5073250c4d61b70f7ec075e82a91fbf446f63c3d
晴天哥哥 <ljfuyuan@qq.com>
2018-08-17 17:06:03 +0800

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -5,7 +5,7 @@ namespace NetProto
         public const int CONNECTION_TIMEOUT = 3000;
         // Size of receive buffer.
         public const int BUFFER_SIZE = 1024;
-        public const int HEADER_SIZE = 2;
+        public const int HEADER_SIZE = 4;
         public const string SALT = "DH";
         public const int DH1BASE = 3;
         public const int DH1PRIME = 0x7FFFFFC3;
